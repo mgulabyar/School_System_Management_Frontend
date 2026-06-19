196bcf9257410a113d848461c7cd322b6beda125
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-19 12:32:30 +0500

    Setup in Academic file

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/set-state-in-effect */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect, useCallback } from "react";
 import {
   Box,
